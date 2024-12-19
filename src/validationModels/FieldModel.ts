a126dc08c0a81fa602b5c6f4856faa6d8c812ee3
isgleb <71262423+isgleb@users.noreply.github.com>
2024-12-20 00:31:38 +0300

    little refactoring

--- src/validationModels/FieldModel.ts
+++ src/validationModels/FieldModel.ts
@@ -15,26 +15,21 @@ export class FieldModel {
     validationRules: Record<ValidationMessage, ValidationRule>
 
     constructor({label = '', key, defaultValue = '', validationRules}: {label?: string, key?: string, defaultValue?: string, validationRules?: Record<ValidationMessage, ValidationRule>}) {
-        console.log(validationRules)
         this.label = label;
         this.value = defaultValue;
         this.validationErrors = [];
         this.validationRules = validationRules;
         this.key = key
         this.showError = false;
-
-        console.log(this.validationRules)
     }
 
     frontValidate() {
         this.validationErrors = []
-        console.log(this.validationRules)
-        for (const [key, val] of Object.entries(this.validationRules)) {
-            console.log(val)
-            console.log(val(this.value))
-            const error = val(this.value) ? '' : key
-            if (error) {
-                this.validationErrors.push(new ValidationError(error));
+
+        for (const [message, validationRule] of Object.entries(this.validationRules)) {
+
+            if (!validationRule(this.value)) {
+                this.validationErrors.push(new ValidationError(message));
             }
         }
     }
